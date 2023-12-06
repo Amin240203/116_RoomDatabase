71086721d5ac6fa5bbf395ff9f9c885c14553d3a
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2023-12-06 22:25:01 +0700

    Object Create

--- app/src/main/java/com/example/pertemuan10/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pertemuan10/ui/halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.pertemuan10.ui.halaman
 
+import com.example.pertemuan10.R
+import com.example.pertemuan10.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
