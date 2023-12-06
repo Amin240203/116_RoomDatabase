58bef7e79d5b476acbd0687bc9b030f62512efe4
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2023-12-06 21:45:09 +0700

    Create Function

--- app/src/main/java/com/example/pertemuan10/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/model/EntryViewModel.kt
@@ -1,8 +1,36 @@
 package com.example.pertemuan10.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.repositori.RepositoriSiswa
 
 class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIstateSiswa())
+        private set
+    /**
+   Fungsi untuk memvalidasi input
+   */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /*
+    Fungsi untuk menyimpan data yang di-entry
+     */
+    suspend fun saveSiswa(){
+        if (validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
