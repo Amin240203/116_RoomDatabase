14c7eaf4a94ee5e57c1504db59a1e785a7b9c836
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2023-12-06 22:13:45 +0700

    Complete AplikasiSiswa

--- app/src/main/java/com/example/pertemuan10/AplikasiSiswa.kt
+++ app/src/main/java/com/example/pertemuan10/AplikasiSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.pertemuan10
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.pertemuan10.repositori.ContainerApp
+import com.example.pertemuan10.repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    /**
+    AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
