975f7ca5396291f3e5f9fb583bba6e12baed0548
Amin Febryansyah <aminfebryansyah240203@gmail.com>
2023-12-06 21:29:17 +0700

    Create Clss EntryViewModel

--- app/src/main/java/com/example/pertemuan10/model/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/model/EntryViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.pertemuan10.model
 
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan10.repositori.RepositoriSiswa
+
+class EntryViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+}
